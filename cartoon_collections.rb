a62944d14a650297f2c58742d0c3ad63807d1b45
Scott Hoisington <hoyzington@gmail.com>
2019-11-13 09:14:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,22 +1,26 @@
 def roll_call_dwarves(array)
   array.each_with_index { |item, index|
-   puts "#{index+1}. #{item}"
+    puts "#{index+1}. #{item}"
   }
 end
 
 
 def summon_captain_planet(array)
   new = array.map {|element|
-   element = "#{element.capitalize}!"
+    element = "#{element.capitalize}!"
   }
   new
 end
 
 
-
 def long_planeteer_calls(array)
-  
-  
+  array.reduce {|memo, item|
+    if item.size > 4
+      return true
+    else
+      return false
+    end
+  }
 end
 
 
